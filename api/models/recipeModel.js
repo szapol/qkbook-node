5339f213cd13761ceea6a34b64f91d6b1ad86672
szapol <karioker1@gmail.com>
2019-07-23 22:49:05 +0200

    Recipe model overhaul

--- api/models/recipeModel.js
+++ api/models/recipeModel.js
@@ -1,13 +1,9 @@
 import mongoose from 'mongoose';
 var Schema = mongoose.Schema;
 
-var RecipeSchema = new Schema({
-    title: {
-        type: String
-    },
-    url: {
-        type: String
-    }
+var recipeSchema = new Schema({
+    title: String,
+    url: String
 });
 
-module.exports = mongoose.model('Recipes', RecipeSchema);
+module.exports = mongoose.model('Recipes', recipeSchema);
