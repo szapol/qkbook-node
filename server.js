d0e646fcf338b8e9aab1b1c21de0009d0a041ba0
szapol <karioker1@gmail.com>
2019-07-23 21:58:08 +0200

    MongoDB connection

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import express from 'express';
 var app = express();
 var port = process.env.PORT || 3000;
 
+import mongoose from 'mongoose';
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/qkbookdb', { useNewUrlParser: true }); 
+
 import routes from './api/routes/recipeRoutes';
 routes(app);
 
