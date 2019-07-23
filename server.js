8d7917680148d1eb481c8cda90bb7888f716ed08
szapol <karioker1@gmail.com>
2019-07-23 22:48:05 +0200

    Body parser

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+import Recipe from './api/models/recipeModel';
+
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/qkbookdb', { useNewUrlParser: true }); 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 import routes from './api/routes/recipeRoutes';
 routes(app);
