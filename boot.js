66ad78f05b9ece360f4ef7469827f3d8f2e89344
szapol <karioker1@gmail.com>
2019-07-23 22:58:16 +0200

    Unnecessarily complicate abstraction

---
+++ boot.js
@@ -0,0 +1,22 @@
+     import express from 'express';
+
+    var app = express();
+    var port = process.env.PORT || 3000;
+    
+    import mongoose from 'mongoose';
+    import bodyParser from 'body-parser';
+    
+    import Recipe from './api/models/recipeModel';
+    
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost/qkbookdb', { useNewUrlParser: true }); 
+    
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    
+    import routes from './api/routes/recipeRoutes';
+    routes(app);
+    
+    app.listen(port);
+    
+    console.log('API server started on port ' + port);
